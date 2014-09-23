b27de5f51a03d719ff1a5069100b19abbeb39ca6
gkiryaziev <linko@ukr.net>
2014-09-23 15:50:15 +0300

    1.0

--- js/main.js
+++ js/main.js
@@ -4,8 +4,12 @@ var app = angular.module('testapp', ['ngRoute']);
 app.config(function($routeProvider) {
 	$routeProvider
 		.when('/', {
-			templateUrl: 'views/index.html',
+			templateUrl: 'views/main.html',
 			controller: 'MainController'
+		})
+		.when('/login', {
+			templateUrl: 'views/login.html',
+			controller: 'LoginController'
 		})
 		.when('/select', {
 			templateUrl: 'views/select.html',
@@ -24,6 +28,12 @@ app.controller('MainController', function($scope) {
 	$scope.info = 'Это главнейшая страница!!!';
 });
 
+app.controller('LoginController', function($scope) {
+	$scope.login = function() {
+		console.log('hello admin');
+	};
+});
+
 app.controller('SelectController', function($scope) {
 	$scope.items = [
 		'Иванов',
@@ -31,7 +41,6 @@ app.controller('SelectController', function($scope) {
 		'Сидоров'
 	];
 });
-
 
 app.controller('AddController', function($scope) {
 	$scope.info = 'Сдесь вы можете добавить абонента.';
